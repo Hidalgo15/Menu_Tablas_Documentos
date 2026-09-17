eb45858cc08bb4fbd413109c058e7b81ca4ec9a2
IsaacEffect <isaacmartinez9875@gmail.com>
2026-09-17 13:38:20 -0400

    Ajuste de tarjetas

--- MenuDeDocumentos/obj/Debug/net8.0/MenuDeDocumentos.AssemblyInfo.cs
+++ MenuDeDocumentos/obj/Debug/net8.0/MenuDeDocumentos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MenuDeDocumentos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba613525bb59f610799991dfa9c262ae51ca1079")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5db545fd689e609fef2764d5dbb1870e2c78894f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MenuDeDocumentos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MenuDeDocumentos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
